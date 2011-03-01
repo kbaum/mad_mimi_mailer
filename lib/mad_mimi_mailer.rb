f7216b360d050b1aae5f93141309f3f9acbd3b24
Karl <karl.baum@weshop.us>
2011-03-01 16:40:17 -0500

    added check_supressed

--- lib/mad_mimi_mailer.rb
+++ lib/mad_mimi_mailer.rb
@@ -3,8 +3,8 @@ require "net/http"
 require "net/https"
 
 class MadMimiMailer < ActionMailer::Base
-  VERSION = '0.0.8'
-  SINGLE_SEND_URL = 'https://madmimi.com/mailer'
+  VERSION              = '0.0.8'
+  SINGLE_SEND_URL      = 'https://madmimi.com/mailer'
 
   InstanceVarsToIgnore = ['action_has_layout',
                           'view_context_class',
@@ -16,9 +16,9 @@ class MadMimiMailer < ActionMailer::Base
                           'unconfirmed',
                           'config']
 
-  @@api_settings = {}
+  @@api_settings       = {}
   cattr_accessor :api_settings
-  
+
   @@default_parameters = {}
   cattr_accessor :default_parameters
 
@@ -47,12 +47,20 @@ class MadMimiMailer < ActionMailer::Base
       @hidden = hidden
     end
   end
-  
+
   def unconfirmed(value = nil)
     if value.nil?
       @unconfirmed
     else
       @unconfirmed = value
+    end
+  end
+
+  def check_suppressed(check_suppressed = nil)
+    if check_suppressed.nil?
+      @check_suppressed
+    else
+      @check_suppressed = check_suppressed
     end
   end
 
@@ -69,18 +77,21 @@ class MadMimiMailer < ActionMailer::Base
     end
 
     def deliver_mimi_mail(method, *parameters)
-      mail = new
+      mail             = new
       mail.action_name = method
-      message = mail.__send__(method, *parameters)
+      message          = mail.__send__(method, *parameters)
 
       # if mail.use_erb
       #   mail.create!(method, *parameters)
       # end
 
+      debugger
+
       return unless perform_deliveries
 
       if delivery_method == :test
-        deliveries << (mail.mail ? mail.mail : mail)
+        deliveries << message
+        #deliveries << (mail.mail ? mail.mail : mail)
       else
         if (all_recipients = mail.recipients).is_a? Array
           all_recipients.each do |recipient|
@@ -95,31 +106,32 @@ class MadMimiMailer < ActionMailer::Base
 
     def call_api!(mail, message, method)
       params = {
-        'username' => api_settings[:username],
-        'api_key' =>  api_settings[:api_key],
-        'promotion_name' => mail.promotion || method.to_s.sub(/^mimi_/, ''),
-        'recipients' =>     serialize(message['to'].to_s),
-        'subject' =>        message['subject'].to_s,
-        'bcc' =>            serialize(message['bcc'] ? message['bcc'].to_s : default_parameters[:bcc]),
-        'from' =>           (message['from'] ? message['from'].to_s : default_parameters[:from]),
-        'hidden' =>         serialize(mail.hidden)
+          'username'       => api_settings[:username],
+          'api_key'        => api_settings[:api_key],
+          'promotion_name' => mail.promotion || method.to_s.sub(/^mimi_/, ''),
+          'recipients'     => serialize(message['to'].to_s),
+          'subject'        => message['subject'].to_s,
+          'bcc'            => serialize(message['bcc'] ? message['bcc'].to_s : default_parameters[:bcc]),
+          'from'           => (message['from'] ? message['from'].to_s : default_parameters[:from]),
+          'hidden'         => serialize(mail.hidden)
       }
 
       params['unconfirmed'] = '1' if mail.unconfirmed
+      params['check_suppressed'] = '1' if mail.check_suppressed
 
       if mail.use_erb
         if message.parts.any?
           params['raw_plain_text'] = content_for(message, "text/plain")
-          params['raw_html'] = content_for(message, "text/html") { |html| validate(html.body.to_s) }
+          params['raw_html']       = content_for(message, "text/html") { |html| validate(html.body.to_s) }
         else
           validate(message.body.to_s)
           params['raw_html'] = message.body.to_s
         end
       else
         stringified_default_body = (default_parameters[:body] || {}).stringify_keys!
-        stringified_mail_body = (mail.instance_values.reject{|k| k[0] == '_' || InstanceVarsToIgnore.index(k)})
-        body_hash = stringified_default_body.merge(stringified_mail_body)
-        params['body'] = body_hash.to_yaml
+        stringified_mail_body    = (mail.instance_values.reject { |k| k[0] == '_' || InstanceVarsToIgnore.index(k) })
+        body_hash                = stringified_default_body.merge(stringified_mail_body)
+        params['body']           = body_hash.to_yaml
       end
 
       response = post_request do |request|
@@ -127,21 +139,21 @@ class MadMimiMailer < ActionMailer::Base
       end
 
       case response
-      when Net::HTTPSuccess
-        response.body
-      else
-        response.error!
+        when Net::HTTPSuccess
+          response.body
+        else
+          response.error!
       end
     end
 
     def content_for(mail, content_type)
-      part = mail.parts.detect {|p| p.content_type == content_type }
+      part = mail.parts.detect { |p| p.content_type == content_type }
       if part
         yield(part) if block_given?
         part.body
       end
     end
-    
+
     def validate(content)
       unless content.include?("[[peek_image]]") || content.include?("[[tracking_beacon]]")
         raise ValidationError, "You must include a web beacon in your Mimi email: [[peek_image]]"
@@ -149,10 +161,10 @@ class MadMimiMailer < ActionMailer::Base
     end
 
     def post_request
-      url = URI.parse(SINGLE_SEND_URL)
+      url     = URI.parse(SINGLE_SEND_URL)
       request = Net::HTTP::Post.new(url.path)
       yield(request)
-      http = Net::HTTP.new(url.host, url.port)
+      http         = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
       http.start do |http|
         http.request(request)
@@ -161,19 +173,20 @@ class MadMimiMailer < ActionMailer::Base
 
     def serialize(recipients)
       case recipients
-      when String
-        recipients.blank? ? nil : recipients
-      when Array
-        recipients.join(", ")
-      when NilClass
-        nil
-      else
-        raise "Please provide a String or an Array for recipients or bcc."
+        when String
+          recipients.blank? ? nil : recipients
+        when Array
+          recipients.join(", ")
+        when NilClass
+          nil
+        else
+          raise "Please provide a String or an Array for recipients or bcc."
       end
     end
   end
 
-  class ValidationError < StandardError; end
+  class ValidationError < StandardError;
+  end
 end
 
 # Adding the response body to HTTPResponse errors to provide better error messages.
